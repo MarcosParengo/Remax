d4c122a5cc432f5cb017ae1d0572e4477c17bac9
MarcosParengo <marcos.sk8.parengo@gmail.com>
2020-10-11 16:18:38 -0300

    Medua qy=uery agentes y formulario listo(Funcion constructora simple y compleja segun el mdia query listener). 7/9 media querys listos

--- Pagina remax/assets/scripts/main.js
+++ Pagina remax/assets/scripts/main.js
@@ -7,24 +7,32 @@ var aver;
 
 $(document).ready(function () {
     var mediaqueryList = window.matchMedia("(max-width: 992px)");
+    var mediaqueryListAgent = window.matchMedia("(max-width: 1200px)");
+
     containerAgent = $("#containerAgent")
     var team = "assets/data/agent.json"
     containerValores = $("#containerValores")
     var perfil = "assets/data/perfil.json"
     containerPropuesta = $("#containerPropuesta")
     var propuesta = "assets/data/propuesta.json"
-    LoadJson(team, 1)
     LoadJson(perfil, 2)
     
-    if(mediaqueryList.matches) {
+    if(mediaqueryList.matches) {//Primer carga segun el tamano (Propuesta)
         LoadJson(propuesta, 3, 1)
         console.log("menos de 992")
-      }else{
+    }else{
         LoadJson(propuesta, 3, 0)
         console.log("mas de 992") 
-      }
+    }
+    if(mediaqueryListAgent.matches) {//Primer carga segun el tamano (Agentes)
+        LoadJson(team, 1,1)
+        console.log("menos de 1200")
+    }else{
+        LoadJson(team, 1,0)
+        console.log("mas de 1200") 
+    }
     
-    mediaqueryList.addListener( function(EventoMediaQueryList) {
+    mediaqueryList.addListener( function(EventoMediaQueryList) {//cambio de la construccion segun los cambios del tamano(Propuesta)
         if(mediaqueryList.matches) {
             LoadJson(propuesta, 3,1)
             console.log("se fue a menos de 992")
@@ -32,8 +40,18 @@ $(document).ready(function () {
             LoadJson(propuesta, 3,0)
             console.log("se pasa de 992")
         }
-});
+    }); 
+    mediaqueryListAgent.addListener( function(EventoMediaQueryList) {
+        if(mediaqueryListAgent.matches) {
+            LoadJson(team, 1,1)
+            console.log("se fue a menos de 1200")
+        }else{
+            LoadJson(team, 1,0)
+            console.log("se pasa de 1200")
+        }
+    }); 
 })
+
 function LoadJson(url, which,MQ) {
     $.ajax({
         method: "get",
@@ -43,7 +61,7 @@ function LoadJson(url, which,MQ) {
         obj = data
         switch (which) {
             case 1:
-                fillContainerAgent(obj);
+                fillContainerAgent(obj,MQ);
                 break
             case 2:
                 fillContainerValores(obj);
@@ -56,65 +74,6 @@ function LoadJson(url, which,MQ) {
         .fail(function (error) {
             console.log(error)
         })
-}
-function fillContainerAgent(obj) {
-    containerAgent.html("")
-    contador = 0;
-    obj.forEach(function (obj, index) {
-        containerAgent.append(`<div class=" col-sm-6 col-md-2  col-xl-2 ">
-                    <article class="agent">
-                            <a href=${obj.link}
-                            target="_blank"><img src="${obj.image}"
-                            width="100%"></a>
-                            <h2 style="margin-top:1em">${obj.name}</h2>
-                            <h2>${obj.phone}</h2>
-                    </article>
-                </div>`)
-        contador++
-        /*
-        if(contador==11){
-            containerAgent.append(`<div class="w-100"></div>`)
-            contador=0
-        }
-        */
-        /*
-         if(contador==2){
-             containerAgent.append(`<div class="w-100"></div>`)
-         }else if(contador==13){
-             containerAgent.append(`<div class="w-100"></div>`)  
-         }else if(contador==19){
-             contador=0
-         }
-         */
-        /*
-         if(contador==5){
-             containerAgent.append(`<div class="w-100"></div>`)
-         }else if(contador==11){
-             contador=0
-         }*/
-        if (contador == 3) {
-            containerAgent.append(`<div class="w-100"></div>`)
-        } else if (contador == 8) {
-            containerAgent.append(`<div class="w-100"></div>`)
-        } else if (contador == 19) {
-            containerAgent.append(`<div class="w-100"></div>`)
-        }
-
-
-    })
-}
-function fillContainerValores(obj) {
-    containerValores.html("");
-    obj.forEach(function (obj, index) {
-        containerValores.append(`<div class=" col-xs-2 col-sm-4 col-md-3  col-xl-2 ">
-                    <article class="agent" >
-                        <img src="${obj.image}"
-                        <div>
-                            <h2>${obj.name}</h2>
-                        </div>
-                    </article>
-                </div>`);
-    });
 }
 function fillContainerPropuesta(obj,MQ) {
     contador = 0;
@@ -156,3 +115,76 @@ function fillContainerPropuesta(obj,MQ) {
         </div>`)
     });}
 }
+function fillContainerValores(obj) {
+    containerValores.html("");
+    obj.forEach(function (obj, index) {
+        containerValores.append(`<div class=" col-xs-2 col-sm-4 col-md-3  col-xl-2 ">
+                    <article class="agent" >
+                        <img src="${obj.image}"
+                        <div>
+                            <h2>${obj.name}</h2>
+                        </div>
+                    </article>
+                </div>`);
+    });
+}
+function fillContainerAgent(obj,MQ) {
+    containerAgent.html("")
+    contador = 0;
+    if(MQ==0){//Mayor a 1200, construccion compleja
+        obj.forEach(function (obj, index) {
+            containerAgent.append(`<div class=" col-sm-6 col-md-4  col-xl-2 ">
+                        <article class="agent">
+                                <a href=${obj.link}
+                                target="_blank"><img src="${obj.image}"
+                                width="100%"></a>
+                                <h2 style="margin-top:1em">${obj.name}</h2>
+                                <h2>${obj.phone}</h2>
+                        </article>
+                    </div>`)
+            contador++
+            if (contador == 3) {
+                containerAgent.append(`<div class="w-100"></div>`)
+            } else if (contador == 8) {
+                containerAgent.append(`<div class="w-100"></div>`)
+            } else if (contador == 19) {
+                containerAgent.append(`<div class="w-100"></div>`)
+            }
+            /*
+            if(contador==11){
+                containerAgent.append(`<div class="w-100"></div>`)
+                contador=0
+            }
+            */
+            /*
+            if(contador==2){
+                containerAgent.append(`<div class="w-100"></div>`)
+            }else if(contador==13){
+                containerAgent.append(`<div class="w-100"></div>`)  
+            }else if(contador==19){
+                contador=0
+            }
+            */
+            /*
+            if(contador==5){
+                containerAgent.append(`<div class="w-100"></div>`)
+            }else if(contador==11){
+                contador=0
+            }*/
+        })
+    }  else{//menor a 1200, construccion simple
+        console.log("construccion simple")
+        obj.forEach(function (obj, index) {
+            containerAgent.append(`<div class=" col-sm-4 col-md-3  col-xl-2 ">
+                        <article class="agent">
+                                <a href=${obj.link}
+                                target="_blank"><img src="${obj.image}"></a>
+                                <h2 style="margin-top:1em">${obj.name}</h2>
+                                <h2>${obj.phone}</h2>
+                        </article>
+                    </div>`)
+            contador++
+        })
+    }
+}
+
